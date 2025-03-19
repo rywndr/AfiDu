5df7e441c7d0c0b87c726d5d8c41e3022a3c1e94
sudodot <haikhalroy@gmail.com>
2025-03-19 10:27:53 +0700

    feat: added cards and student counter for dashboard

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from students.models import Student
+
 
 # Create your views here.
 @login_required
 def dashboard(request):
-    return render(request, "dashboard/dashboard.html")
+    student_count = Student.objects.count()
+    context = {"student_count": student_count}
+    return render(request, "dashboard/dashboard.html", context)
