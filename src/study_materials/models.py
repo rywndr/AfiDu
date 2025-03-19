f8ab8786b88283ae89feafad2e05fa88cd6c6f56
sudodot <haikhalroy@gmail.com>
2025-03-19 22:51:41 +0700

    feat: autogen thumbnail for study materials if file is PDF

--- src/study_materials/models.py
+++ src/study_materials/models.py
@@ -1,6 +1,9 @@
+import os
+
 from django.conf import settings
 from django.core.validators import FileExtensionValidator
 from django.db import models
+from pdf2image import convert_from_path
 
 
 # Create your models here.
@@ -16,13 +19,10 @@ class StudyMaterial(models.Model):
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
     file = models.FileField(
         upload_to="study_materials/",
-        validators=[
-            FileExtensionValidator(
-                allowed_extensions=[
-                    "pdf",
-                ]
-            )
-        ],  # "ppt", "pptx"])],
+        validators=[FileExtensionValidator(allowed_extensions=["pdf"])],
+    )
+    thumbnail = models.ImageField(
+        upload_to="study_material_thumbnails/", blank=True, null=True
     )
     uploaded_at = models.DateTimeField(auto_now_add=True)
     edited_at = models.DateTimeField(auto_now=True)
@@ -40,7 +40,33 @@ class StudyMaterial(models.Model):
     def get_file_extension(self):
         return self.file.name.split(".")[-1].lower()
 
+    def save(self, *args, **kwargs):
+        # save file dulu
+        super().save(*args, **kwargs)
+        # generate thumbnail pakai pdf2image jika file nya == PDF dan thumbnail nya belum ada
+        if self.file and self.get_file_extension() == "pdf" and not self.thumbnail:
+            try:
+                pdf_path = self.file.path
+                output_dir = os.path.join(
+                    settings.MEDIA_ROOT, "study_material_thumbnails"
+                )
+                os.makedirs(output_dir, exist_ok=True)
+                thumb_filename = f"{self.pk}_thumb.jpg"
+                thumb_path = os.path.join(output_dir, thumb_filename)
+                # ubah page pertama dari PDF ke jpeg
+                pages = convert_from_path(pdf_path, first_page=1, last_page=1)
+                if pages:
+                    pages[0].save(thumb_path, "JPEG")
+                    self.thumbnail = os.path.join(
+                        "study_material_thumbnails", thumb_filename
+                    )
+                    super().save(update_fields=["thumbnail"])
+            except Exception as e:
+                print("Error generating thumbnail:", e)
+
     def delete(self, *args, **kwargs):
-        # delete uploaded file dari filesystem
+        # delete file dan thumbnail dari filesystem
         self.file.delete(save=False)
+        if self.thumbnail:
+            self.thumbnail.delete(save=False)
         super().delete(*args, **kwargs)
