99525bf46cae40d0e93cb76f49d049e235404c9e
rywndr <haikhalroy@gmail.com>
2025-03-28 23:39:37 +0700

    refactor: refactored dashboard app views to use generic views and also set the greetings once per session

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
-from django.views import View
+from django.views.generic import TemplateView
 import random
 
 from students.models import Student
 
 # Create your views here.
-class DashboardView(LoginRequiredMixin, View):
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = "dashboard/dashboard.html"
+    
+    # greetings untuk dashboard
     greetings = [
         "Welcome back!",
         "Good to see you again!",
@@ -16,25 +18,27 @@ class DashboardView(LoginRequiredMixin, View):
         "Ready to tackle the day?",
         "Hope you're having a great day!",
     ]
-
     user_greetings = [
-            "Welcome back",
-            "Hello",
-            "Hi there",
-            "Greetings",
-            "Good to see you",
+        "Welcome back",
+        "Hello",
+        "Hi there",
+        "Greetings",
+        "Good to see you",
     ]
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        session = self.request.session
 
-    greeting = random.choice(greetings)
-    user_greeting = random.choice(user_greetings)
-
-    def get(self, request, *args, **kwargs):
-        student_count = Student.objects.count()
-        context = {
-            "greeting": self.greeting,
-            "user_greeting": self.user_greeting,
-            "student_count": student_count,
-            "active_tab_title": "Dashboard",
-            "active_tab_icon": "fa-tachometer-alt",
-        }
-        return render(request, "dashboard/dashboard.html", context)
+        # set greeting once per session
+        if "dashboard_greeting" not in session:
+            session["dashboard_greeting"] = random.choice(self.greetings)
+        if "dashboard_user_greeting" not in session:
+            session["dashboard_user_greeting"] = random.choice(self.user_greetings)
+        
+        context["greeting"] = session["dashboard_greeting"]
+        context["user_greeting"] = session["dashboard_user_greeting"]
+        context["student_count"] = Student.objects.count()
+        context["active_tab_title"] = "Dashboard"
+        context["active_tab_icon"] = "fa-tachometer-alt"
+        return context
