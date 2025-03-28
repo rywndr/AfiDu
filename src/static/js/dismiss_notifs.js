0367d1d9a25821b9ccd80ce806b0582a7f414043
rywndr <haikhalroy@gmail.com>
2025-03-28 20:56:53 +0700

    fix: fixed close button in notifs not working/added event listener for the button that i forgot to do xD

--- src/static/js/dismiss_notifs.js
+++ src/static/js/dismiss_notifs.js
@@ -1,16 +1,21 @@
 (function() {
-    function dismissNotification(){
-      const container = document.getElementById('notification-container');
-      if(container){
-        container.firstElementChild.classList.add('opacity-0');
-        setTimeout(() => {
-          container.remove();
-        }, 300);
-      }
+  function dismissNotification(){
+    const container = document.getElementById('notification-container');
+    if(container){
+      container.firstElementChild.classList.add('opacity-0');
+      setTimeout(() => {
+        container.remove();
+      }, 300);
     }
-    
-    setTimeout(() => {
-      dismissNotification();
-    }, 3000);
-  })();
+  }
+
+  // dissmiss button event listener
+  const dismissButton = document.getElementById('dismiss-btn');
+  if(dismissButton){
+    dismissButton.addEventListener('click', dismissNotification);
+  }
   
+  setTimeout(() => {
+    dismissNotification();
+  }, 3000);
+})();
