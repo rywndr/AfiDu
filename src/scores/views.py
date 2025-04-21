6499f25ac0f99b3b939870d5cae5b8b3530ec6a3
sudodot <haikhalroy@gmail.com>
2025-04-21 22:13:52 +0700

    misc: adjusted semester label in score list

--- src/scores/views.py
+++ src/scores/views.py
@@ -22,7 +22,7 @@ class ScoreContextMixin:
             "active_tab_title": "Scores",
             "active_tab_icon": "fa-chart-bar",
             "years": range(2025, 2033),
-            "semesters": [("mid", "MID"), ("final", "FINALS")],
+            "semesters": [("mid", "MID"), ("final", "FINAL")],
             "categories": [
                 ("reading", "Reading"),
                 ("writing", "Writing"),
